a5d17e6a45bc55d57d0639e1e0935de54a82a45b
ksrajkamal <ksrajkamal231@gmail.com>
2024-05-14 08:11:04 +0530

    add main folder

---
+++ main/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from .views import main_view, home_view, list_view, listing_view, edit_view
+from .views import name
+
+urlpatterns = [
+    path('', main_view, name='main'),
+    path('home/', home_view, name='home'),
+    path('list/', list_view, name='list'),
+    path('listing/<str:id>/', listing_view, name='listing'),
+    path('listing/<str:id>/edit/', edit_view, name='edit'),
+
+]
